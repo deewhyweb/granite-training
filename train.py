e9a929edfbcd179f2a5ae4b58d0d9058789c1971
Philip <philip@deewhy.ie>
2024-11-12 16:36:09 -0500

    update

--- train.py
+++ train.py
@@ -98,7 +98,7 @@ collator = DataCollatorForCompletionOnlyLM(response_template_ids, tokenizer=toke
 qlora_config = LoraConfig(
     r=16,  # The rank of the Low-Rank Adaptation
     lora_alpha=32,  # Scaling factor for the adapted layers
-    target_modules= ['q_proj', 'k_proj', 'v_proj', 'o_proj'],  # Layer names to apply LoRA to
+    target_modules= ['q_proj', 'k_proj', 'v_proj'],  # Layer names to apply LoRA to
     lora_dropout=0.1,
     bias="none"
 )
