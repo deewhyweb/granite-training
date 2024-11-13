10f3e5ebfd75ba6a86cb4d7e13c7497a9570de68
Philip <philip@deewhy.ie>
2024-11-13 11:04:35 -0500

    adding merging

--- merge.py
+++ merge.py
@@ -4,7 +4,7 @@ from peft import PeftModel, PeftConfig
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 base_model_name = "ibm-granite/granite-3.0-8b-instruct"  # Replace with your base model name
-model = AutoModelForCausalLM.from_pretrained(base_model_name)
+base_model = AutoModelForCausalLM.from_pretrained(base_model_name)
 tokenizer = AutoTokenizer.from_pretrained(base_model_name)
 
 lora_model_path_1 = "./results"
@@ -15,7 +15,8 @@ peft_config_1 = PeftConfig.from_pretrained(lora_model_path_1)
 
 lora_model_1 = PeftModel.from_pretrained(model, lora_model_path_1, peft_config=peft_config_1)
 
+model = PeftModel.from_pretrained(base_model, lora_model_1)
+merged_model = model.merge_and_unload()
 
-merged_model = model.merge_and_unload(lora_model_1)
 
 merged_model.save_pretrained("./merged")
